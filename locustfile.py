92d2df32c0740150ff02eb1daedb067d9030031d
Onkar JINDE <71400604+onkarjinde1@users.noreply.github.com>
2024-01-07 00:25:23 +0100

    Locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -4,5 +4,32 @@ class MyUser(HttpUser):
     wait_time = between(1, 3)
 
     @task
-    def invoke_microservice(self):
-        self.client.get("/")
+    def numerical_integration_task(self):
+        lower = 0
+        upper = 3.14
+
+        response = self.client.get(f"/numericalintegralservice/{lower}/{upper}")
+
+        # Check if the response is successful (status code 200)
+        if response.status_code == 200:
+            # You can parse the response content if needed
+            integration_results = response.json()
+        else:
+            # Handle non-successful responses as needed
+            print(f"Error: {response.status_code}, {response.text}")
+
+    @task
+    def details_task(self):
+        lower = 0
+        upper = 3.14
+
+        response = self.client.get(f"/numericalintegralservice/{lower}/{upper}/details")
+
+        # Check if the response is successful (status code 200)
+        if response.status_code == 200:
+            # You can parse the response content if needed
+            details_data = response.text
+        else:
+            # Handle non-successful responses as needed
+            print(f"Error: {response.status_code}, {response.text}")
+
